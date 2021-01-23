8dcbfea5524e616faf06dda462c8235996d509b8
lfchesebrough <Lewis.Chesebrough@accolade.com>
2021-01-22 19:01:38 -0800

    Add files via upload

---
+++ email_LFC.py
@@ -0,0 +1,31 @@
+import smtplib
+
+gmail_user = 'lfchesebrough@gmail.com'
+gmail_password = 'mmklndcfdjkpfdyc'
+
+# send an email from above gmail user account, 
+def sendEmail(message):
+    sent_from = gmail_user
+    to = ['lfchesebrough@gmail.com', 'lx829382@dal.ca']
+    subject = 'VACCINE SHIFT AVAILABLE'
+    body = message
+
+    email_text = """\
+    From: %s
+    To: %s
+    Subject: %s
+
+    %s
+    """ % (sent_from, ", ".join(to), subject, body)
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(gmail_user, gmail_password)
+        server.sendmail(sent_from, to, email_text)
+        server.close()
+
+        print('Email sent!')
+    except:
+        print('Something went wrong...')
+
